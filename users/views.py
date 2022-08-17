466f53aac65e7e29252f1c737f7a098cd710fbfc
Veronica Lourens <Veronica@lourens.cloud>
2022-08-17 17:08:07 +0000

    Create userprofile form, views, template & urls.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    """
+    To render user profile page with user's personal information.
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
--- veroperfume/urls.py
+++ veroperfume/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('', include('users.urls')),
 
 
     
