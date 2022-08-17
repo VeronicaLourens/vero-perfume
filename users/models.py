885ea6519495da810c03f5e18eb2d80288527d81
Veronica Lourens <Veronica@lourens.cloud>
2022-08-17 16:22:27 +0000

    Create user profile model & migrate changes.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+# The user profile model is from CI Boutique Ado project.
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_city = models.CharField(max_length=40, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.user.username
+        
