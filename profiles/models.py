46508bab647ee16dfee3739054b2f943cb892d97
Veronica Lourens <Veronica@lourens.cloud>
2022-08-18 10:14:56 +0000

    Update django user authentication.

--- profiles/models.py
+++ profiles/models.py
@@ -32,8 +32,8 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile.
     """
-    if created:
-        UserProfile.objects.create(user=instance)
+    #if created:
+    UserProfile.objects.create(user=instance)
    
-    instance.userprofile.save()
+    #instance.userprofile.save()
 
