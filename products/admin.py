3c65d8d9235926190b4b8acd47b1358523b961b9
Veronica Lourens <Veronica@lourens.cloud>
2022-08-19 18:05:33 +0000

    Create product, category, brand models & register to admin.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
 
-# Register your models here.
+# Register models to admin
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """
+    To create product's catogory.
+    """
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True, 
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Brand(models.Model):
+    """
+    To create product's brand.
+    """
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True, 
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    To create product with the product details.
+    """
+
+    MEN = 'Men'
+    WOMEN = 'Women'
+
+    GENDER_CHOICES = [
+        (MEN, 'Men'),
+        (WOMEN, 'Women')
+    ]
+
+    sku = models.CharField(
+        max_length=254,
+        null=True, blank=True
+    )
+    name = models.CharField(
+        max_length=254
+        )
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    brand = models.ForeignKey(
+        'Brand',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+    gender = models.CharField(
+        choices=GENDER_CHOICES,
+        max_length=24,
+        default=''
+    )
+    description = models.TextField()
+    image_url = models.URLField(
+        max_length=1024,
+        null=True,
+    )
+    image = models.ImageField(
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
