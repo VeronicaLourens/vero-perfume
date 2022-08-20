a3153f81e0158fcd32eeb4f0c00fa3b1023f0554
Veronica Lourens <Veronica@lourens.cloud>
2022-08-20 15:04:47 +0000

    Add product detail views, styling & url links.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def products(request):
-    """ To render all products"""
+    """ To render all products page."""
 
     products = Product.objects.all()
 
@@ -11,3 +11,17 @@ def products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+
+def product_detail(request, product_id):
+    """ To render the product detail page."""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
