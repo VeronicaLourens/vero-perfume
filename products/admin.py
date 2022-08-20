1da132fea1ce42c5b8529a4e49bc96695b7e47c3
Veronica Lourens <Veronica@lourens.cloud>
2022-08-20 07:13:18 +0000

    Add admin register in admin.py

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,26 @@ from .models import Category, Brand, Product
 
 # Register models to admin
 
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('name','brand', 'category')
+    list_filter = ('name', 'brand')
+    search_fields = ('brand', 'gender')
+    
