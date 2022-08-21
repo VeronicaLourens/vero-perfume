28f4aa9b41799aca7bb3045d1bedaeee7a577067
Veronica Lourens <Veronica@lourens.cloud>
2022-08-21 04:06:54 +0000

    Add search query funtionality & products link on nav links.

--- products/views.py
+++ products/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.contrib import messages
+from django.db.models import Q
 
 def products(request):
-    """ To render all products page."""
+    """ 
+    To render all products page.
+    Sorting, filtering and search queries.
+    """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
