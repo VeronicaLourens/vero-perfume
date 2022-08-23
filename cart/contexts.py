1a686e91e3d2d7479a35f362bc45de7a4f2a76cc
Veronica Lourens <Veronica@lourens.cloud>
2022-08-23 18:37:53 +0000

    Add cart context processor & add grand total to header.

--- cart/contexts.py
+++ cart/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def cart_contents(request):
     """
     To display the content in the shopping cart.
@@ -11,6 +12,29 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        if isinstance(quantity, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += quantity * product.price
+            product_count += quantity
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in quantity['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
