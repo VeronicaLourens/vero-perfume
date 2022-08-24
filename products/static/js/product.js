edaab24e15aebb1eaef38d1bab6614535627e14f
Veronica Lourens <Veronica@lourens.cloud>
2022-08-24 17:02:24 +0000

    Testing to render the product price.

--- products/static/js/product.js
+++ products/static/js/product.js
@@ -2,7 +2,7 @@ let ids=["30ml", "50ml", "75ml", "100ml"];
 let SizeDropDown = document.getElementById("product-size");
 
 SizeDropDown.onchange = function () {
-    for (var x = 0; x < ids.length; x++) {
+    for (let x = 0; x < ids.length; x++) {
         document.getElementById(ids[x]).style.display = "none";
     }
     document.getElementById(this.value).style.display = "block";
