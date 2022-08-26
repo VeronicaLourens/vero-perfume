7c0cef78e61e1139b99e1d68154e2a05deb58d88
Veronica Lourens <Veronica@lourens.cloud>
2022-08-26 17:15:35 +0000

    Add remove product functionality & cart tools.

---
+++ cart/templatetags/__int__.py
@@ -0,0 +1,4 @@
+"""
+To ensure that this directory is treated as a Python package.
+To make the cart tools module available for imports and to use in templates.
+"""
--- cart/views.py
+++ cart/views.py
@@ -86,7 +86,7 @@ def update_cart(request, item_id):
 
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-        
+
     cart = request.session.get('cart', {})
 
     if size:
@@ -129,4 +129,44 @@ def update_cart(request, item_id):
     request.session['cart'] = cart
 
     return redirect(reverse('view_cart'))
-    
+
+
+def remove_from_cart(request, item_id):
+    """
+    To remove product from the cart.
+    """
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        size = None
+
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        cart = request.session.get('cart', {})
+
+        if size:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+
+            messages.success(
+                request, 
+                f'Removed {product.name} (size {size}) from your cart'
+            )
+        else:
+            cart.pop(item_id)
+            messages.success(
+                request, 
+                f'Removed {product.name} from your cart'
+            )
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:  
+        messages.error(
+            request, 
+            f'Error removing item: {e}'
+        )
+        return HttpResponse(status=500)
+
