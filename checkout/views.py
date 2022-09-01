114a8eef045cf3716190958bbecb5d5db9f4f662
Veronica Lourens <Veronica@lourens.cloud>
2022-09-01 16:51:48 +0000

    Add stripe keys & script to checkout app.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LRUHXEIvO02kFtr7f8sfa2dqmz8hT0kCmYpQywhUnmIzIFGaTlSXszsx5XLIYHlKC9ISjcOMiKqGWGmWmCflKMz00s3x2m78M',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
