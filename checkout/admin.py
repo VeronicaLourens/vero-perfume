67b23b9855359be929c77a45f12f04c35ae3191b
Veronica Lourens <Veronica@lourens.cloud>
2022-09-04 07:27:14 +0000

    Add code to complete webhook handler.

--- checkout/admin.py
+++ checkout/admin.py
@@ -22,6 +22,8 @@ class OrderAdmin(admin.ModelAdmin):
         'delivery_cost',
         'order_total',
         'grand_total',
+        'original_cart',
+        'stripe_pid'
     )
 
     fields = (
@@ -39,6 +41,8 @@ class OrderAdmin(admin.ModelAdmin):
         'delivery_cost',
         'order_total',
         'grand_total',
+        'original_cart',
+        'stripe_pid'
     )
 
     list_display = (
