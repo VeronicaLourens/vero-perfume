ac737719411a22c050c519e969654a3862bc1137
Veronica Lourens <Veronica@lourens.cloud>
2022-09-08 10:59:42 +0000

    Add my order list page.

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history/<order_number>',
-        views.order_history, name='order_history'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
     path('personal_details/', views.personal_details, name='personal_details'),
+    
 ]
