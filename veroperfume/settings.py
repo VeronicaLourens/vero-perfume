ecfa2c21398f76df6524277b31e3a2d5546bbe29
Veronica Lourens <Veronica@lourens.cloud>
2022-09-08 11:52:26 +0000

    Set up sending emails via verified gmail account.

--- veroperfume/settings.py
+++ veroperfume/settings.py
@@ -111,8 +111,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -229,4 +227,14 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 CSRF_TRUSTED_ORIGINS = ['https://veroperfume.herokuapp.com/']
 
-DEFAULT_FROM_EMAIL = 'veroperfume@example.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'veroperfume@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
