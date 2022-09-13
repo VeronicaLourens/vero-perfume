6e6ca445f41fb48165d00d4c575fce2129857c78
Veronica Lourens <Veronica@lourens.cloud>
2022-09-13 18:18:28 +0000

    Register wishlit to admin & make migrations.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, WishList, WishListItem
 
 
 @admin.register(UserProfile)
@@ -8,3 +8,8 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'default_phone_number')
     list_filter = ('user',)
     search_fields = ('user', 'default_phone_number')
+
+
+admin.site.register(WishList)
+
+admin.site.register(WishListItem)
