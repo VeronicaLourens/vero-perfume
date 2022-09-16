45d6a705787bf4473ee8dba7e2daa9d47f39f144
Veronica Lourens <Veronica@lourens.cloud>
2022-09-16 09:18:00 +0000

    Create contact app and models.

---
+++ contact/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class ContactUs(models.Model):
+    """
+    To contact the shop owner.
+    """
+    email = models.EmailField()
+    content = models.TextField(
+        max_length=250,
+        null=False,
+        blank=False
+    )
+    date_sent = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        """
+        Meta data for the contact.
+        """
+        ordering = ('-date_sent')
+
+    def __str__(self):
+        return f'{self.email}'
