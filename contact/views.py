e92f0ce99a35650be27fa38a33041fcf0fb9840a
Veronica Lourens <Veronica@lourens.cloud>
2022-09-17 05:04:41 +0000

    Complete contact functionality.

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from .forms import ContactUsForm
 
 
@@ -18,14 +19,23 @@ def contact(request):
             email = contact_form.cleaned_data['email']
             message = contact_form.cleaned_data['message']
 
-            contact = contact_form.save(commit=False)
-            contact.save()
+            html = render_to_string(
+                'contact/email.html', {
+                    'full_name': full_name,
+                    'email': email,
+                    'message': message
+                }
+            )
+
+            contact_form = contact_form.save(commit=False)
+            contact_form.save()
 
             send_mail(
                 'Contact us',
                 'Thank you for contacting us!',
                 'info@veroperfume.com',
-                ['info@gmail.com']
+                ['user@email.com'],
+                html_message=html
             )
             messages.success(request, 'Successfully sent the message!')
             return redirect('home')
