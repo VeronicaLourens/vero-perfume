55cdd803a2c70cd95d3bbaf65de7f58ae2425c08
Veronica Lourens <Veronica@lourens.cloud>
2022-09-18 12:52:24 +0000

    Add policy view template url in home app.

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('policy/', views.policy, name='policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ def home(request):
     """ To render the home page """
 
     return render(request, 'home/index.html')
+
+
+def policy(request):
+    """To render the policy page"""
+    return render(request, 'home/policy.html')
