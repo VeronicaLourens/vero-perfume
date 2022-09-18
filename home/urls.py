366e17620f6662ed0bbcca1ca584e1cd8a685d30
Veronica Lourens <Veronica@lourens.cloud>
2022-09-18 17:47:23 +0000

    Delete policy view template url in home app & create policy modal.

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('policy/', views.policy, name='policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,8 +5,3 @@ def home(request):
     """ To render the home page """
 
     return render(request, 'home/index.html')
-
-
-def policy(request):
-    """To render the policy page"""
-    return render(request, 'home/policy.html')
