840f3faae4593e237eaa8ca38a447448127e9cb8
Veronica Lourens <Veronica@lourens.cloud>
2022-09-26 18:11:20 +0000

    Add home view & contact form test.

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .forms import ContactUsForm
 
-# Create your tests here.
+
+class TestContactUsForm(TestCase):
+    """
+    To test contact form
+    """
+    def test_contact_us_form(self):
+        """
+        To check the contact us form
+        """
+        form = ContactUsForm({
+            'full_name': 'Test full name',
+            'email': 'test@veroperfume.com',
+            'message': 'Welcome to Vero Perfume online store!',
+        })
+        self.assertTrue(form.is_valid())
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestHomeViews(TestCase):
+    """
+    To test the home page views
+    """
+    def test_home_page(self):
+        """
+        To check the home page 
+        """
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
