fcc05628c9da64be533dc3ee96d0cba90fffba42
Veronica Lourens <Veronica@lourens.cloud>
2022-10-14 06:36:06 +0000

    Add comments to the custom storages file

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,15 @@
+"""
+Storage location for static files
+"""
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """Static files on AWS"""
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """Media files on AWS"""
     location = settings.MEDIAFILES_LOCATION
